5c30f400a1b2fe511769368c53131937ae4339c9
joran <joran.huibers@gmail.com>
2019-07-12 22:51:26 +0200

    first commit

---
+++ src/main/java/me/tacticaldev/tacticallobby/TacticalLobby.java
@@ -0,0 +1,17 @@
+package me.tacticaldev.tacticallobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TacticalLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
